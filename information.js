f38d3986be524e09efe3582ffc988b7f24d5cfc7
JeonHongYeol <daruken@JeonHongYeols-MacBook-Pro.local>
2016-03-27 15:22:55 +0900

    Add api by shlee.

--- information.js
+++ information.js
@@ -45,3 +45,26 @@ function showContactUs(){
         window.location.reload();
     }
 }
+
+function showHelp(){
+    var divElement = document.createElement("DIV");
+    var textNode = document.createTextNode("Just do it.");
+    divElement.setAttribute("style", "height:50px;background-color:black;color:whitesmoke;font-family:monospace;font-size:25px;padding:50px;text-align:center;");
+        
+    divElement.appendChild(textNode);
+    document.body.appendChild(divElement);
+    
+    if(Boolean(showFlag == false))
+    {
+        divElement.appendChild(textNode);
+        document.body.appendChild(divElement);    
+        showFlag = true;
+    }
+    else
+    {
+        divElement.removeChild(textNode);
+        document.body.removeChild(divElement);
+        showFlag = false;
+        window.location.reload();
+    }
+}
